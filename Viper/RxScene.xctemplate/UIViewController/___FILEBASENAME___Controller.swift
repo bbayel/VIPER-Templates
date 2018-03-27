d53bdc85e679a7574c5c823e120bd6efdae590e1
Benjamin Bayel <bbayel@mystudiofactory.com>
2018-03-27 10:17:38 +0200

    ad Rx VIPER templates

---
+++ Viper/RxScene.xctemplate/UIViewController/___FILEBASENAME___Controller.swift
@@ -0,0 +1,50 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//
+
+
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+protocol ___VARIABLE_sceneName___Intents : class {
+	func loadIntent() -> Observable<Void> 
+    func display(viewModel : ___VARIABLE_sceneName___ViewModel)
+}
+
+
+
+class ___VARIABLE_sceneName___Controller : ___VARIABLE_viewControllerSubclass___, ___VARIABLE_sceneName___Intents {
+    
+    var presenter : ___VARIABLE_sceneName___ModuleInterface!
+    
+    
+    //MARK:-  View LifeCycle
+        deinit {
+        print("Deinit \(self)")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter.attach()
+        
+    }
+    
+
+    //MARK:- RxIntents
+    func loadIntent() -> Observable<Void> {
+    	return Observable.just(())
+    }
+
+    //MARK:- Display
+    func display(viewModel: ___VARIABLE_sceneName___ViewModel) {
+
+    }
+    
+}
---
+++ Viper/RxScene.xctemplate/UIViewController/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,58 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//
+
+import Foundation
+import RxSwift
+
+
+
+struct ___VARIABLE_sceneName___ViewModel {
+
+    
+}
+
+protocol ___VARIABLE_sceneName___ModuleInterface : class {
+    func attach()
+}
+
+
+class  ___VARIABLE_sceneName___Presenter : ___VARIABLE_sceneName___ModuleInterface {
+    private let bag = DisposeBag()
+    ///Use the scheduler for debouce, Throttle, etc. The scheduler can be set in the constructor to facilitate tests.
+    private let scheduler : SchedulerType
+    
+    private let router : ___VARIABLE_sceneName___RouterInput
+    private let interactor : ___VARIABLE_sceneName___InteractorInput
+    private weak var viewController : ___VARIABLE_sceneName___Intents?
+    
+    
+    init(router : ___VARIABLE_sceneName___RouterInput,
+         interactor : ___VARIABLE_sceneName___InteractorInput,
+         viewController : ___VARIABLE_sceneName___Intents,
+         scheduler : SchedulerType = MainScheduler.instance ) {
+        self.router = router
+        self.interactor = interactor
+        self.viewController = viewController
+        self.scheduler = scheduler
+    }
+    
+    deinit {
+        print("Deinit \(self)")
+    }
+
+    
+    func attach() {
+
+        guard let viewController = viewController else { return }
+        
+    }
+    
+
+    
+}
